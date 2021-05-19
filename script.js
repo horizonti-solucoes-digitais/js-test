7348ac5fac872d3608a7437809e30891db4bb10e
Oryange <oryangestrifezze@gmail.com>
2021-05-19 09:14:55 -0300

    Adiciona form login no header

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+
+function validateLogin() {
+  const email = document.querySelector('.email');
+  const inputemail = email.value;
+  const emailFormate = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/.test(inputemail);
+  if (!emailFormate || pass.value !== '123456') {
+    email.value = ''
+    alert('Login ou senha inválidos. Tente novamente!');
+    return false;
+  } else {
+    alert('Bem vindo, pesquise o projeto desejado!');
+    return true;
+  }
+}
+
 function createItemList({ language, description }) {
   const li = document.createElement('li');
   const conteinerList = document.querySelector('ol');
@@ -6,28 +21,29 @@ function createItemList({ language, description }) {
   conteinerList.appendChild(li);
   return li;
 }
-// function renderCurriculum() {
-//   const fullForm = document.getElementById('full-form');
 
-//   fullForm.addEventListener('submit', function(event){
-//     event.preventDefault();
-//     if(checkData() && validateEmail()) {
+function renderPortfolio() {
+  const fullForm = document.querySelector('form');
+
+  fullForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    if(checkData() && validateEmail()) {
            
-//       const formelements = document.querySelectorAll('input');
-//         for (let index = 0; index < formelements.length; index +=1) {
-//         if (formelements[index].type === 'radio' && !formelements[index].checked) {
-//           continue;
-//         } 
-//         const inputUser = formelements[index].value;
-//         const formData = document.getElementById('form-attached');
-//         const fullForm = document.createElement('div');
-//         fullForm.id = 'div';
-//         fullForm.innerHTML = inputUser;
-//         formData.appendChild(fullForm);     
-//       } 
-//     } 
-//   })
-// }
+      const formelements = document.querySelectorAll('input');
+        for (let index = 0; index < formelements.length; index +=1) {
+        if (formelements[index].type === 'radio' && !formelements[index].checked) {
+          continue;
+        } 
+        const inputUser = formelements[index].value;
+        const formData = document.getElementById('form-attached');
+        const fullForm = document.createElement('div');
+        fullForm.id = 'div';
+        fullForm.innerHTML = inputUser;
+        formData.appendChild(fullForm);     
+      } 
+    } 
+  })
+}
 
 function fetchRepo() {
   const endpoint = 'https://api.github.com/users/ORYANGE/repos';
@@ -39,7 +55,7 @@ function fetchRepo() {
     const button = document.querySelector('.button-subtmit');
     button.addEventListener('click', function(event) {
       event.preventDefault();
-      conteinerList.appendChild(createItemList({language, description}));
+      conteinerList.appendChild(createItemList({language, description})); 
       });
   }));
 }
